ca39b79ecdceaa7887e515dfff856b4618779374
Inrepublica <duvalmickael@gmail.com>
2019-11-29 16:57:13 +0100

    Update mod and add patches
    Update mod description and title, add more patches.

---
+++ FR_Unit_Patches/Addons/FR_Unit_Patches/config.cpp
@@ -0,0 +1,72 @@
+class CfgPatches {
+ 	class FR_Unit_Patches
+ 	{
+ 		units[] = {};
+ 		weapons[] = {};
+ 		requiredVersion = 1.0;
+ 		requiredAddons[] = {};
+ 	};
+ };
+ 
+ class CfgUnitInsignia {
+	class legion_legio_patria_nostra {
+		displayName = "LEGION Legio Patria Nostra";
+		texture = "\FR_Unit_Patches\data\legion_legio_patria_nostra.paa";
+		author = "Inrepublica";
+	};
+	class legion_diable_rit_jaune {
+		displayName = "LEGION Le diable rit jaune";
+		texture = "\FR_Unit_Patches\data\legion_diable_rit_jaune.paa";
+		author = "Inrepublica";
+	};
+	class legion_2rep_couleur {
+		displayName = "LEGION 2e REP couleur";
+		texture = "\FR_Unit_Patches\data\legion_2rep_couleur.paa";
+		author = "Inrepublica";
+	};
+	class legion_infanterie_losange {
+		displayName = "LEGION losange de bras infanterie";
+		texture = "\FR_Unit_Patches\data\legion_infanterie_losange.paa";
+		author = "Inrepublica";
+	};
+	class legion_cavalerie_losange {
+		displayName = "LEGION losange de bras cavalerie";
+		texture = "\FR_Unit_Patches\data\legion_cavalerie_losange.paa";
+		author = "Inrepublica";
+	};
+	class legion_argent_arc_bras {
+		displayName = "LEGION arc de bras ARGENT";
+		texture = "\FR_Unit_Patches\data\legion_argent_arc_bras.paa";
+		author = "Inrepublica";
+	};
+	class legion_or_arc_bras {
+		displayName = "LEGION arc de bras OR";
+		texture = "\FR_Unit_Patches\data\legion_or_arc_bras.paa";
+		author = "Inrepublica";
+	};
+	class centre_instruction_parachutiste {
+		displayName = "Centre d'Instruction Parachutiste";
+		texture = "\FR_Unit_Patches\data\centre_instruction_parachutiste.paa";
+		author = "Inrepublica";
+	};
+	class france_basse_visibilite {
+		displayName = "FRANCE basse visibilité";
+		texture = "\FR_Unit_Patches\data\france_basse_visibilite.paa";
+		author = "Inrepublica";
+	};
+	class france_couleur {
+		displayName = "FRANCE couleur";
+		texture = "\FR_Unit_Patches\data\france_couleur.paa";
+		author = "Inrepublica";
+	};
+	class france_couleur_dore {
+		displayName = "FRANCE couleur doré";
+		texture = "\FR_Unit_Patches\data\france_couleur_dore.paa";
+		author = "Inrepublica";
+	};
+	class france_desert {
+		displayName = "FRANCE désert";
+		texture = "\FR_Unit_Patches\data\france_desert.paa";
+		author = "Inrepublica";
+	};
+ };
---
+++ FR_Unit_Patches/mod.cpp
@@ -0,0 +1,28 @@
+/// Name of your mod
+name = "French Unit Patches";
+/// Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+picture = "Logo_FR_Unit_Patches_Picture.paa"; 
+/// Display next to the item added by the mod
+logoSmall = "Logo_FR_Unit_Patches_Small.paa";
+/// Logo displayed in the main menu
+logo = "Logo_FR_Unit_Patches.paa";
+/// When the mouse is over, in the main menu
+logoOver = "Logo_FR_Unit_Patches_Over.paa";
+/// Website URL, that can accessed from the expansions menu
+action = "https://github.com/inrepublica/FR_Unit_Patches";
+/// Tool tip displayed when the mouse is left over, in the main menu
+tooltipOwned = "French Unit Patches";
+/// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] = 
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+/// Overview text, displayed from the extension menu
+overview = "Various Patches for French Unit.";
+/// Hide the extension name 
+hideName = 0;
+/// Hide the extension menu
+hidePicture = 0;
